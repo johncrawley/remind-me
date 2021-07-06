7b0583aae63df10eb35be772bb56e51d839c9019
johncrawley <johncrawley1@gmail.com>
2021-07-06 16:56:05 +0100

    Initial commit

---
+++ app/src/main/java/com/jcrawley/remindme/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jcrawley.remindme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
